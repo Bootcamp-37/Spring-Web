8e13d8558411811ddf48b0d4d0532c2aad2ebd16
Zakky F H <zakkyhermansyah@gmail.com>
2020-10-20 17:11:28 +0700

    update paling baru

--- testcrud/testcrud/src/main/java/bootcamp37/testcrud/TestcrudApplication.java
+++ testcrud/testcrud/src/main/java/bootcamp37/testcrud/TestcrudApplication.java
@@ -10,7 +10,7 @@ public class TestcrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestcrudApplication.class, args);
-                System.out.println("jalan");
+                System.out.println("server jalan");
 	}
         @Bean
         public RestTemplate getRestTemplate(){
--- testcrud/testcrud/src/main/java/bootcamp37/testcrud/controllers/PersonApiController.java
+++ testcrud/testcrud/src/main/java/bootcamp37/testcrud/controllers/PersonApiController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
